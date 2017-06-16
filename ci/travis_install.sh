80f64d5aea06f0a2740814eb431f3b3cbeaf0ebc
tenfy <tenfyzhong@qq.com>
2017-06-16 23:29:04 +0800

    parallel test.

--- ci/travis_install.sh
+++ ci/travis_install.sh
@@ -1,20 +1,24 @@
 #!/bin/bash -
 set -e
 
-if [[ $TRAVIS_OS_NAME == 'osx'  ]]; then
-    brew update
-    brew install neovim/neovim/neovim
+
+if [[ "$VIM_NAME" == 'nvim' ]]; then
+    if [[ "$TRAVIS_OS_NAME" == 'osx'  ]]; then
+        curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz
+        tar xzf nvim-macos.tar.gz
+    else
+        sudo add-apt-repository ppa:neovim-ppa/unstable -y
+        sudo apt-get update -y
+        sudo apt-get install neovim -y
+    fi
 else
-    sudo add-apt-repository ppa:neovim-ppa/unstable -y
-    sudo apt-get update -y
-    sudo apt-get install neovim -y
+    wget https://codeload.github.com/vim/vim/tar.gz/v7.4.774
+    tar xzf v7.4.774
+    cd vim-7.4.774
+    ./configure --prefix="$HOME/vim" \
+        --enable-fail-if-missing \
+        --with-features=huge
+    make -j 2
+    make install
 fi
 
-wget https://codeload.github.com/vim/vim/tar.gz/v7.4.774
-tar xzf v7.4.774
-cd vim-7.4.774
-./configure --prefix="$HOME/vim" \
-    --enable-fail-if-missing \
-    --with-features=huge
-make -j 2
-make install
