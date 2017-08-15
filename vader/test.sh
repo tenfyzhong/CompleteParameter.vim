bf65d87a2eb31099ff2feefd23e4860e060c097e
tenfy <tenfyzhong@qq.com>
2017-08-15 20:48:53 +0800

    Use absolutely path.

--- vader/test.sh
+++ vader/test.sh
@@ -5,8 +5,8 @@ vim -Nu <(cat << VIMRC
 filetype off
 set rtp+=~/.vim/bundle/vader.vim
 set rtp+=~/.vim/bundle/CompleteParameter.vim
-runtime! cm_parser/*.vim
+runtime! ~/.vim/bundle/CompleteParameter.vim/cm_parser/*.vim
 filetype plugin indent on
 syntax enable
-VIMRC) -c 'Vader! *' > /dev/null
+VIMRC) -c 'Vader! ~/.vim/bundle/CompleteParameter.vim/vader/*' > /dev/null
 
